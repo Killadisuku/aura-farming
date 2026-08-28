9e6bbd4e81c284bb59fc613747a07996824c5fd5
Killadisuku <139642675+Killadisuku@users.noreply.github.com>
2026-08-29 00:11:01 +0400

    Import Google button CSS

--- boot.jsx
+++ boot.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './aura.css'
+import './google.css'
 import App from './AuraApp.jsx'
 
 createRoot(document.getElementById('root')).render(
